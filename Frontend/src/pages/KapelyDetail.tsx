e3bad6cb404c367e7f004e62824f21be983980de
Petr Foldyna <petr.foldyna@bluesoft.cz>
2023-05-17 23:16:34 +0200

    implemented react markdown 2

--- Frontend/src/pages/KapelyDetail.tsx
+++ Frontend/src/pages/KapelyDetail.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import useFetch from '../hooks/UseFetch';
 import { useParams } from 'react-router';
 import { useLocation } from 'react-router-dom';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import LinkBack from '../components/core/LinkBack';
+import SvgIcon from '../components/core/SvgIcon';
 import MainImage from '../components/MainImage';
 
 interface BandProps {
@@ -10,6 +12,7 @@ interface BandProps {
     Name: string;
     Description?: string;
     Image?: any;
+    blogText: any,
     createdAt: string;
     publishedAt: string;
     updatedAt: string;
@@ -33,7 +36,29 @@ export default function KapelaDetail() {
           <MainImage image={data?.Image.data.attributes.url} title={data?.Name} />
         </div>
     </section>
-      <div>{data?.Name}</div>
+    <section className="section section--top section--bottom">
+        <div className="container">
+            <div className="blog">
+                <div className="blog__text">
+                    <ReactMarkdown className="blog__rich-text">
+                      {data?.blogText}
+                    </ReactMarkdown>
+                    <div className="blog__audio-section">
+                        <h4>Ukázka z tvorby:</h4>                    
+                    </div>
+                    <h4>Video ukázka:</h4>
+                    <div className="blog__video-section">
+                    </div>
+                </div>
+                <div className="blog__social">
+                    <span>Sociální sítě:</span>
+                    <a target="_blank" href="https://www.facebook.com/375mnm" className="blog__social-item"><SvgIcon svgName={"fb"}></SvgIcon></a>
+                    <a target="_blank" href="https://www.instagram.com/375_m.n.m/" className="blog__social-item"><SvgIcon svgName={"instagram"}></SvgIcon></a>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=_4C7Zr0qL8s&ab_channel=TheAttics" className="blog__social-item"><SvgIcon svgName={"yt"}></SvgIcon></a>
+                </div>
+            </div>
+        </div>
+    </section>
     </>
   );
 }
