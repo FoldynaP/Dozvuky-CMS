00f0fb5c22897a7197e2c1282f9a77d823f54dce
petr.foldyna <petr.foldyna@bluesoft.cz>
2023-06-01 14:59:26 +0200

    production entry app.js

--- Frontend/config/webpack.prod.js
+++ Frontend/config/webpack.prod.js
@@ -5,7 +5,9 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(webpackConfig, {
 	mode: 'production',
-
+	entry: {
+		app: './dist/js/app.js', // entry point for your React app
+	  },
 	optimization: {
 		minimize: true,
 		minimizer: [new TerserPlugin()],
