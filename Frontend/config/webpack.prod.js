eea01182ff0f3ac2e0c0736dce7efdc2af74e2b7
petr.foldyna <petr.foldyna@bluesoft.cz>
2023-06-01 15:09:04 +0200

    webpack prod revert changes

--- Frontend/config/webpack.prod.js
+++ Frontend/config/webpack.prod.js
@@ -5,9 +5,6 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(webpackConfig, {
 	mode: 'production',
-	entry: {
-		app: './dist/js/app.js', // entry point for your React app
-	  },
 	optimization: {
 		minimize: true,
 		minimizer: [new TerserPlugin()],
